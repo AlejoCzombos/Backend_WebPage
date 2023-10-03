f15b8559031591673e4b24810d691b0a86185304
Alejo <alejoczombos@gmail.com>
2023-10-03 16:10:28 -0300

    Configuracion

--- src/main/java/com/backend/Login/Config/SecurityConfig.java
+++ src/main/java/com/backend/Login/Config/SecurityConfig.java
@@ -25,7 +25,7 @@ public class SecurityConfig {
                 .cors(cors -> cors.disable())
                 .csrf(csrf -> csrf.disable()) //Desabilitar la proteccion csrf de Spring. Seguridad para las request POST
                 .authorizeHttpRequests(authRequest ->
-                        authRequest.requestMatchers("/auth/**", "/api/**", "/**").permitAll() //Autorizar todos las urls auth para que sean publicas
+                        authRequest.requestMatchers("/auth/**", "/api/**", "/api/students/**", "/api/parents/**").permitAll() //Autorizar todos las urls auth para que sean publicas
                                 .anyRequest().authenticated()
                 )
                 .sessionManagement(sessionManager ->
