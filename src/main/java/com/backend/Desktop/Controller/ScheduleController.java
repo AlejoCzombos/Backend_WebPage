1ecbb7696d3ddedeff012bfac147f7d39f2de773
Alejo <alejoczombos@gmail.com>
2023-10-16 22:24:45 -0300

    Nueva build

--- src/main/java/com/backend/Desktop/Controller/ScheduleController.java
+++ src/main/java/com/backend/Desktop/Controller/ScheduleController.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+@RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/schedules")
 public class ScheduleController {
--- src/main/java/com/backend/Desktop/DTO/NoteDTO.java
+++ src/main/java/com/backend/Desktop/DTO/NoteDTO.java
@@ -1,12 +1,13 @@
 package com.backend.Desktop.DTO;
 
-import com.backend.Desktop.Entity.Student;
 import lombok.Data;
 
 @Data
 public class NoteDTO {
 
     private String class_name;
+
+    private Integer id;
 
     private Integer numeric_note_1;
     private Integer numeric_note_2;
